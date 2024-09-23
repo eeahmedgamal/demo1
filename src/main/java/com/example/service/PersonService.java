9edb8edfa599119a9c25e486a54498b559fcabbe
eeahmedgamal <137240499+eeahmedgamal@users.noreply.github.com>
2024-09-23 17:12:07 +0300

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -39,6 +39,14 @@ public class PersonService {
 		p.setFirstName("AbdulHalim");
 		p.setLastName("Mahmoud");
 		persons.add(p);
+
+		
+		p = new Person();
+		p.setId("5");
+		p.setAge(55);
+		p.setFirstName("Basam");
+		p.setLastName("mokhtar");
+		persons.add(p);
 		
 	}
 	public Person getPerson(String id) {
